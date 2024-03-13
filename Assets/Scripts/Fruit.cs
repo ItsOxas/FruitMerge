e8485552f79eab45a51489c27d880e010f7203cd
ItsOxas <121440551+ItsOxas@users.noreply.github.com>
2024-03-13 17:58:07 +0200

    Added more fruits but game bugs out

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,33 +1,59 @@
+using System.Collections.Generic;
 using System.Xml.Linq;
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
     static public Rigidbody2D rb;
     public bool fruitLanded;
+    public int fruitTier;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (gameObject.transform.position.y > FruitSpawner.spawningHeight)
+        {
+            Destroy(gameObject);
+        }
     }
     static public void fruitFall(Vector2 mousePosition)
     {    
         if (FruitSpawner.fruitState == "inAir")
         {
             rb.gravityScale = 2;
-            rb.position = new Vector2(mousePosition.x, 3);
+            rb.position = new Vector2(mousePosition.x, FruitSpawner.spawningHeight);
             FruitSpawner.fruitState = "falling";
         }
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!fruitLanded && collision.gameObject.tag != "sideBorder") 
+        if (gameObject.transform.position.y < FruitSpawner.spawningHeight)
         {
-            fruitLanded = true;
-            FruitSpawner.fruitState = "landed";
-            print("collision");
+
+            if (!fruitLanded && collision.gameObject.tag != "sideBorder")
+            {
+                fruitLanded = true;
+                FruitSpawner.fruitState = "landed";
+            }
+                if (collision.gameObject.tag == "Fruit")
+                {
+                var otherFruit = collision.gameObject.GetComponent<Fruit>();
+                int otherFruitTier = otherFruit.fruitTier;
+                    if (otherFruitTier == fruitTier)
+                    {
+                        FruitSpawner.fruitState = "merging";
+                        FruitSpawner.mergeTier = fruitTier;
+                        FruitSpawner.mergePos = gameObject.transform.position;
+                        Destroy(gameObject);
+                        Destroy(collision.gameObject);
+                }
+                }
         }    
     }
 }
--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -1,11 +1,22 @@
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
 {
+    public List<GameObject> spawnerFruits;
     public List<GameObject> fruits;
     static public string fruitState = "inAir";
     static public Vector2 mousePos;
+    static public float spawningHeight = 8f;
+    static public Vector2 mergePos;
+    static public int mergeTier;
+
+    private void Start()
+    {
+        SpawnFruit();
+    }
 
     public void Update()
     {
@@ -15,17 +26,33 @@ public class FruitSpawner : MonoBehaviour
             if (fruitState == "inAir")
             {
                 Fruit.fruitFall(mousePos);
-            }    
+            }
         }
         if (fruitState == "landed")
         {
             SpawnFruit();
-            fruitState = "inAir";
         }
+        if (fruitState == "merging")
+        {
+            MergeFruits(mergeTier, mergePos);
+        }
+        print(fruitState);
     }
     public void SpawnFruit()
     {
-        Instantiate(fruits[Random.Range(0, fruits.Count)],new Vector2(0, 3), Quaternion.identity);
+        Instantiate(spawnerFruits[Random.Range(0, spawnerFruits.Count)], new Vector2(0, spawningHeight), Quaternion.identity);
+        fruitState = "inAir";
     }
-
+    void MergeFruits(int tier, Vector2 pos)
+    {
+        if (tier <= fruits.Count)
+        {
+            var fruit = Instantiate(fruits[tier + 1], pos, Quaternion.identity);
+            var fruitRb = fruit.GetComponent<Rigidbody2D>();
+            fruit.GetComponent<Fruit>().fruitLanded = true;
+            fruitRb.gravityScale = 2;
+            SpawnFruit();
+        }
+    }
 }
+
